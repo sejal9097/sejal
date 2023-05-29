b906c9ef74cc41cdd1a1be08813aea10363ebc34
sejal9097 <134928577+sejal9097@users.noreply.github.com>
2023-05-29 15:36:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+let inC = true;
+let weather = {
+    apiKey: "3baf2ef7fb70c5c33b252209ff0b2b49",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      localStorage["temp"]=temp;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+
+  const convert = document.getElementById('convert');
+  convert.addEventListener('click',() => {
+    if(inC){
+      document.querySelector(".temp").innerText=((9*localStorage["temp"])/5)+32 + " F";
+      inC=!inC;
+    }else{
+      document.querySelector(".temp").innerText= localStorage["temp"]+" C";
+      inC=!inC;
+    }
+  })
+  
+  // weather.fetchWeather("Dhanbad");
